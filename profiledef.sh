ff45834cfab9b6bbbf70c63e2b086b5fdf3ba749
Jacob McSwain <jacob@mcswain.dev>
2023-04-24 23:59:41 -0500

    Updates

--- profiledef.sh
+++ profiledef.sh
@@ -8,14 +8,13 @@ iso_application="Arch Linux baseline McSwain"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
+airootfs_image_type="ext4+squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
+  ["/usr/local/bin/install-script"]="0:0:755"
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:0750"
   ["/root/.ssh"]="0:0:0700"
