6a124de8f82df56d687c7ac92bd03688d29da36d
Jacob McSwain <jacob@mcswain.dev>
2023-05-03 15:57:50 -0500

    Update keyring and install base

--- airootfs/root/alis/files/usr/local/bin/provision-script.sh
+++ airootfs/root/alis/files/usr/local/bin/provision-script.sh
@@ -8,8 +8,10 @@ SigLevel = Required TrustedOnly
 Server = https://raw.githubusercontent.com/USA-RedDragon/arch-packages/bins/\$arch
 EOF
 
-curl -fSsLo /tmp/usa-reddragon-keyring-20230501-5-any.pkg.tar.zst https://github.com/USA-RedDragon/arch-packages/raw/bins/x86_64/usa-reddragon-keyring-20230501-5-any.pkg.tar.zst
-pacman --noconfirm -U /tmp/usa-reddragon-keyring-20230501-5-any.pkg.tar.zst
-rm -f /tmp/usa-reddragon-keyring-20230501-5-any.pkg.tar.zst
+curl -fSsLo /tmp/usa-reddragon-keyring-20230501-6-any.pkg.tar.zst https://github.com/USA-RedDragon/arch-packages/raw/bins/x86_64/usa-reddragon-keyring-20230501-6-any.pkg.tar.zst
+pacman --noconfirm -U /tmp/usa-reddragon-keyring-20230501-6-any.pkg.tar.zst
+rm -f /tmp/usa-reddragon-keyring-20230501-6-any.pkg.tar.zst
+
+pacman --noconfirm -Sy usa-reddragon-base
 
 rm -f $0
